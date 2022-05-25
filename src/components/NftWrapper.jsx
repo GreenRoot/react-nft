fdda250cc2c0be1020f54bfacd796b9d28ebf946
GreenRoot <1.ooo.ooo@bk.ru>
2022-05-26 02:54:35 +0300

    NFT cards added to the array

--- src/components/NftWrapper.jsx
+++ src/components/NftWrapper.jsx
@@ -1,5 +1,17 @@
 import card_01 from '../img/card_01.webp';
 
+// Cards info
+const cards = [
+	{ img: card_01, price: '35,21', name: 'Stoned Ninjas #2592', link: '#' },
+	{ img: card_01, price: '25,55', name: 'Stoned Ninjas #2593', link: '#' },
+	{ img: card_01, price: '15,12', name: 'Stoned Ninjas #2594', link: '#' },
+	{ img: card_01, price: '55,25', name: 'Stoned Ninjas #2595', link: '#' },
+	{ img: card_01, price: '31,02', name: 'Stoned Ninjas #2596', link: '#' },
+	{ img: card_01, price: '38,20', name: 'Stoned Ninjas #2597', link: '#' },
+	{ img: card_01, price: '88,25', name: 'Stoned Ninjas #2598', link: '#' },
+	{ img: card_01, price: '11,25', name: 'Stoned Ninjas #2599', link: '#' },
+];
+
 function NftWrapper() {
 	return (
 		<div style={{ marginTop: 50 + 'px' }} className="nft-wrapper">
@@ -7,62 +19,15 @@ function NftWrapper() {
 				Back to table
 			</a>
 			<div className="cards">
-				<a href="#" className="card">
-					<div className="card__img">
-						<img src={card_01} alt="nft" />
-					</div>
-					<div className="card__price">35,25 $</div>
-					<div className="card__name">Stoned Ninjas #2592</div>
-				</a>
-				<a href="#" className="card">
-					<div className="card__img">
-						<img src={card_01} alt="nft" />
-					</div>
-					<div className="card__price">35,25 $</div>
-					<div className="card__name">Stoned Ninjas #2592</div>
-				</a>
-				<a href="#" className="card">
-					<div className="card__img">
-						<img src={card_01} alt="nft" />
-					</div>
-					<div className="card__price">35,25 $</div>
-					<div className="card__name">Stoned Ninjas #2592</div>
-				</a>
-				<a href="#" className="card">
-					<div className="card__img">
-						<img src={card_01} alt="nft" />
-					</div>
-					<div className="card__price">35,25 $</div>
-					<div className="card__name">Stoned Ninjas #2592</div>
-				</a>
-				<a href="#" className="card">
-					<div className="card__img">
-						<img src={card_01} alt="nft" />
-					</div>
-					<div className="card__price">35,25 $</div>
-					<div className="card__name">Stoned Ninjas #2592</div>
-				</a>
-				<a href="#" className="card">
-					<div className="card__img">
-						<img src={card_01} alt="nft" />
-					</div>
-					<div className="card__price">35,25 $</div>
-					<div className="card__name">Stoned Ninjas #2592</div>
-				</a>
-				<a href="#" className="card">
-					<div className="card__img">
-						<img src={card_01} alt="nft" />
-					</div>
-					<div className="card__price">35,25 $</div>
-					<div className="card__name">Stoned Ninjas #2592</div>
-				</a>
-				<a href="#" className="card">
-					<div className="card__img">
-						<img src={card_01} alt="nft" />
-					</div>
-					<div className="card__price">35,25 $</div>
-					<div className="card__name">Stoned Ninjas #2592</div>
-				</a>
+				{cards.map((index) => (
+					<a href={index.link} className="card">
+						<div className="card__img">
+							<img src={index.img} alt={index.name} />
+						</div>
+						<div className="card__price">{`$ ${index.price}`}</div>
+						<div className="card__name">{index.name}</div>
+					</a>
+				))}
 			</div>
 			<button className="btn load-more">Load more</button>
 		</div>
