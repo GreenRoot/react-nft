3c5a4066e8f14c5c9571c2a1671041cb20389ea8
GreenRoot <1.ooo.ooo@bk.ru>
2022-05-25 22:26:18 +0300

    Form validation ready

--- src/components/NftCalc.jsx
+++ src/components/NftCalc.jsx
@@ -1,4 +1,19 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
 function NftCalc() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			sum: 1000,
+			risk: 'LowRisk',
+		},
+	});
+	console.log(errors);
+
 	return (
 		<div className="nft-calc">
 			<div className="nft-calc__prewiew">
@@ -20,19 +35,27 @@ function NftCalc() {
 					</svg>
 				</div>
 			</div>
-			<form action="#" className="calc">
+			<form
+				onSubmit={handleSubmit((data) => {
+					alert(`Data ready 
+sum: ${data.sum * 1}
+risk: ${data.risk}`);
+				})}
+				className="calc">
 				<h2 className="calc__title">Assemble NFT&nbsp;portfolio</h2>
 				<label className="calc__label">
 					<div className="calc__text">How much money are you willing to invest?</div>
 					<div className="calc__input">
+						<p className="calc__input-error">{errors.sum?.message}</p>
 						<span className="calc__input-prefix">$</span>
 						<input
+							{...register('sum', {
+								required: 'This is required!',
+								min: { value: 10, message: 'Min value is $10' },
+								max: { value: 10000000, message: 'Max value is $10.000.000' },
+							})}
 							className="calc__input-number"
 							type="number"
-							defaultValue="1000"
-							step="10"
-							min="10"
-							max="10000000"
 						/>
 					</div>
 				</label>
@@ -41,31 +64,46 @@ function NftCalc() {
 					<div className="calc-radio__inputs">
 						<label className="radio">
 							<input
-								defaultChecked
+								{...register('risk', { required: 'This is required!' })}
 								className="radio__input"
-								id="lowRisk"
 								type="radio"
-								name="risk"
+								value="LowRisk"
 							/>
 							<span className="radio__imput-radio"></span>
 							<span className="radio__label">Low Risk</span>
 						</label>
 						<label className="radio">
-							<input className="radio__input" id="moderate" type="radio" name="risk" />
+							<input
+								{...register('risk', { required: 'This is required!' })}
+								className="radio__input"
+								type="radio"
+								value="Moderate"
+							/>
 							<span className="radio__imput-radio"></span>
 							<span className="radio__label">Moderate</span>
 						</label>
 						<label className="radio">
-							<input className="radio__input" id="high" type="radio" name="risk" />
+							<input
+								{...register('risk', { required: 'This is required!' })}
+								className="radio__input"
+								type="radio"
+								value="High"
+							/>
 							<span className="radio__imput-radio"></span>
 							<span className="radio__label">High</span>
 						</label>
 						<label className="radio">
-							<input className="radio__input" id="veryHigh" type="radio" name="risk" />
+							<input
+								{...register('risk', { required: 'This is required!' })}
+								className="radio__input"
+								type="radio"
+								value="VeryHigh"
+							/>
 							<span className="radio__imput-radio"></span>
 							<span className="radio__label">Very high</span>
 						</label>
 					</div>
+					<p className="calc__input-error">{errors.risk?.message}</p>
 				</div>
 				<button className="btn btn-calc">Calculate</button>
 			</form>
--- src/components/NftWrapper.jsx
+++ src/components/NftWrapper.jsx
@@ -2,14 +2,17 @@ import card_01 from '../img/card_01.webp';
 
 function NftWrapper() {
 	return (
-		<div style={{marginTop: 50 + 'px'}} className="nft-wrapper">
+		<div style={{ marginTop: 50 + 'px' }} className="nft-wrapper">
 			<a href="#" className="back">
 				Back to table
 			</a>
 			<div className="cards">
 				<a href="#" className="card">
 					<div className="card__img">
-						<img src={card_01} alt="nft" />
+						<img
+							src="https://lh3.googleusercontent.com/lmij0QW_Y0bxQSXgM16j8wNcBGgphyrxjplZDqjymrxOT3nrHNtaG-LNrgXyN0b1-Ws2rEGYJRcugmelNrUB9DlZBCQA9ZUg62urDg=w500"
+							alt="nft"
+						/>
 					</div>
 					<div className="card__price">35,25 $</div>
 					<div className="card__name">Stoned Ninjas #2592</div>
