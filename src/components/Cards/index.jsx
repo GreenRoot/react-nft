38e13f62c79268415fa5b6089d1ca720c0f6d742
GreenRoot <1.ooo.ooo@bk.ru>
2022-05-29 09:29:42 +0300

    Card Added check for invalid data

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,13 +1,36 @@
 import styles from './Card.module.sass';
 
-function Card({ id, link, img, name, price }) {
+function Card({
+	id,
+	link,
+	img,
+	name = 'Name not found',
+	price = 'Prise not found',
+	risk = 'Low risk',
+}) {
+	// str "12345669.1" to 12,345,669.10
+	// num 1213215.2521 to 1,213,215.25
+	function toPrice(price) {
+		return parseFloat(price.toString().replaceAll(' ', '').replace(',', '.'))
+			.toFixed(2)
+			.replace(/\d(?=(\d{3})+\.)/g, '$&,');
+	}
+
 	return (
 		<a key={id} target="_blank" href={link} className={styles.card}>
 			<div className={styles.img}>
 				<img src={img + '=w500'} alt={name} />
 			</div>
-			<div className={styles.price}>{`$ ${price.toFixed(2)}`}</div>
-			<div className={styles.name}>{name}</div>
+			<div className={styles.status}>
+				<div title={`$ ${toPrice(price)}`} className={styles.price}>{`$ ${toPrice(price)}`}</div>
+				<div className={styles.risk}>
+					<div>{risk}</div>
+					<div className={styles.riskStatus}></div>
+				</div>
+			</div>
+			<div title={name} className={styles.name}>
+				{name}
+			</div>
 		</a>
 	);
 }
