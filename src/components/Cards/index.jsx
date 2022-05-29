2026d7140498ecd096c35df44a05d789dba855d3
GreenRoot <1.ooo.ooo@bk.ru>
2022-05-29 07:15:45 +0300

    Card component styles rewritten under modules

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,11 +1,13 @@
+import styles from './Card.module.sass';
+
 function Card({ id, link, img, name, price }) {
 	return (
-		<a key={id} target="_blank" href={link} className="card">
-			<div className="card__img">
+		<a key={id} target="_blank" href={link} className={styles.card}>
+			<div className={styles.img}>
 				<img src={img + '=w500'} alt={name} />
 			</div>
-			<div className="card__price">{`$ ${price.toFixed(2)}`}</div>
-			<div className="card__name">{name}</div>
+			<div className={styles.price}>{`$ ${price.toFixed(2)}`}</div>
+			<div className={styles.name}>{name}</div>
 		</a>
 	);
 }
